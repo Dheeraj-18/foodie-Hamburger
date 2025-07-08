981cffcd09167312b0fd955e20f04e188a75a0dc
think2code <dheerajsaraswat03@gmail.com>
2025-07-08 11:41:24 +0530

    feat - add window eventlistener and fix event propogation

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const hamburgerIcon = document.querySelector('.hamburger-menu-container')
 const headerContent = document.querySelector('.header-content')
 const closeIcon = document.querySelector('.close-icon')
 const goToTopBtn = document.querySelector('.go-to-top')
+const nav = document.querySelector('nav')
 
-hamburgerIcon.addEventListener('click', () => {
+hamburgerIcon.addEventListener('click', (e) => {
+  e.stopPropagation()
   headerContent.classList.add('menu-open')
 })
 
 closeIcon.addEventListener('click', () => {
+  headerContent.classList.remove('menu-open')
+})
+
+nav.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
+
+window.addEventListener('click', () => {
   headerContent.classList.remove('menu-open')
 })
 
